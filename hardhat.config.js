cb5b8cf52af2152fae895c4d42530773a66ca2fd
captain_Chris <christian@euphena.com>
2025-11-13 13:07:29 +0700

    Create hardhat.config.jsAdd Hardhat configuration file

---
+++ hardhat.config.js
@@ -0,0 +1,10 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+require("@nomicfoundation/hardhat-toolbox");
+
+module.exports = {
+  solidity: "0.8.20",
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {},
+  },
+};
